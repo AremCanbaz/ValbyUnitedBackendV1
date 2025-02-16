0e50fd018367cc5e25e41f7ff0334b57f6077e69
AC25-C <arem_canbaz@icloud.com>
2025-02-16 13:45:06 +0100

    Initial commit

---
+++ src/main/java/org/example/valbyunitedbackendv1/ValbyUnitedBackendV1Application.java
@@ -0,0 +1,13 @@
+package org.example.valbyunitedbackendv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValbyUnitedBackendV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValbyUnitedBackendV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/valbyunitedbackendv1/ValbyUnitedBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.valbyunitedbackendv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValbyUnitedBackendV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
