fb6183f2cda9b9bffc1aaec85b79156681c69371
AC25-C <151057176+AC25-C@users.noreply.github.com>
2025-03-02 16:53:36 +0100

    Ny funktion afhenter kun kommende kampe, og så er der også blevet tilføjet article, alt vliver sendt som json. alt virker

--- src/main/java/org/example/valbyunitedbackendv1/Controller/ArticleController.java
+++ src/main/java/org/example/valbyunitedbackendv1/Controller/ArticleController.java
@@ -3,6 +3,7 @@ package org.example.valbyunitedbackendv1.Controller;
 import org.example.valbyunitedbackendv1.Entity.Articles;
 import org.example.valbyunitedbackendv1.Service.ArticlesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("article")
 public class ArticleController {
     @Autowired
